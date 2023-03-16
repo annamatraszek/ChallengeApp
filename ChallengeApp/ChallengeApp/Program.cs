d6c03d6a604347851060d79f04a0eebc9809aa38
annamatraszek <anna_matraszek@wp.pl>
2023-03-16 19:54:37 +0100

    zadanie domowe

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Napisz Program.cs 
+// - zmienna z imieniem
+// - zmienna z płcią
+// - zmienna z wiekiem
+// - zweryfikować dane i wyświetlić
+// - jeden z komunikatów:
+// (1) "Kobieta poniżej 30 lat"
+// (2) "Ewa , lat 30"
+// (3) "Niepełnoletni mężczyzna" 
+
+
+string myName = "Anna";
+var plec = "Kobieta";
+int myAge = 25;
+string txt1 = "Kobieta poniżej 30 lat";
+string txt2 = "Kobieta powyżej 30 lat";
+string txt3 = "Niepełnoletni mężczyzna";
+
+
+if (plec == "Kobieta")
+{
+    if (myAge < 30)
+    {
+        Console.WriteLine(txt1);
+        Console.WriteLine(myName + ", lat " + myAge);
+    }
+    else if (myAge >= 30)
+    {
+        Console.WriteLine(txt2);
+        Console.WriteLine(myName + ", lat " + myAge);
+    }
+}
+else
+{
+    Console.WriteLine(txt3);
+}
+
