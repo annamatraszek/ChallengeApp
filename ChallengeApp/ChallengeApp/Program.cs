2e28401699d454ef749cfad355a709e3b6bd964b
annamatraszek <anna_matraszek@wp.pl>
2023-07-07 19:08:53 +0200

    zadanie dzień 11

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -19,20 +19,20 @@ Console.WriteLine($"Average: {statistics.Average:N2}");
 var statistics2 = employee.GetStatisticsWithFor();
 
 Console.WriteLine(" ... GetStatisticsWithFor");
-Console.WriteLine($"Min: {statistics.Min}");
-Console.WriteLine($"Max: {statistics.Max}");
-Console.WriteLine($"Average: {statistics.Average:N2}");
+Console.WriteLine($"Min: {statistics2.Min}");
+Console.WriteLine($"Max: {statistics2.Max}");
+Console.WriteLine($"Average: {statistics2.Average:N2}");
 
 var statistics3 = employee.GetStatisticsWithDoWhile();
 
 Console.WriteLine(" ... GetStatisticsWithDoWhile");
-Console.WriteLine($"Min: {statistics.Min}");
-Console.WriteLine($"Max: {statistics.Max}");
-Console.WriteLine($"Average: {statistics.Average:N3}");
+Console.WriteLine($"Min: {statistics3.Min}");
+Console.WriteLine($"Max: {statistics3.Max}");
+Console.WriteLine($"Average: {statistics3.Average:N3}");
 
 var statistics4 = employee.GetStatisticsWithWhile();
 
 Console.WriteLine(" ... GetStatisticsWithWhile");
-Console.WriteLine($"Min: {statistics.Min}");
-Console.WriteLine($"Max: {statistics.Max}");
-Console.WriteLine($"Average: {statistics.Average:N1}");
+Console.WriteLine($"Min: {statistics4.Min}");
+Console.WriteLine($"Max: {statistics4.Max}");
+Console.WriteLine($"Average: {statistics4.Average:N1}");
